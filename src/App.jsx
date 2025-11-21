84208897748d8c69c1ca263d54f809b0d4881e33
unknown <kumasomeswari@example.com>
2025-11-21 22:33:00 +0530

    reception 21-11-25

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import SignUpPage from "./pages/signup_page/SignUpPage"
 import SignInPage from "./pages/signin_page/SignInPage"
+import DashBoard from "./pages/receptionist_pages/dash_board/DashBoard"
+
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 export default function App(){
 
     return (
-        <BrowserRouter basename="/">
-            <Routes>
-                <Route path="/" element={<SignUpPage />}/>
+        // <BrowserRouter basename="/">
+        //     <Routes>
+        //         <Route path="/" element={<SignUpPage />}/>
 
-                <Route path="/signup" element={<SignUpPage />} />
+        //         <Route path="/signup" element={<SignUpPage />} />
 
-                <Route path="/login" element={<SignInPage />} />
-            </Routes>
-        </BrowserRouter>
-    )
+        //         <Route path="/login" element={<SignInPage />} />
+        //     </Routes>
+        // </BrowserRouter>
+
+        <DashBoard/>
+)
 }
