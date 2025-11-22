95db8e610b1d108926d3bfb9dbaff07b4322899e
Murthy9644 <bvjsmurthy90@gmail.com>
2025-11-22 20:45:00 +0530

    sidebar made reusable

--- src/pages/receptionist_pages/doctor_management/DoctorManagement.jsx
+++ src/pages/receptionist_pages/doctor_management/DoctorManagement.jsx
@@ -1,4 +1,5 @@
 import styles from './dark_doctor_management.module.css';
+import SideBar from '../../../reusable_components/receptionist_sidebar/ReceptionistSideBar'
 
 export default function DoctorManagement(){
 
@@ -33,34 +34,7 @@ export default function DoctorManagement(){
                 </div>
             </div>
 
-            <div className={styles.sidebar}>
-                <div className={styles.ensureTogether}>
-                    <div className={styles.hospitalBranding}>
-                        <div className={styles.logo}>
-                        </div>
-
-                        <div className={styles.hospitalName}>
-                            <span><h4>Lorem ipsum dolor</h4></span>
-
-                            <span><p>Reception desk</p></span>
-                        </div>
-                    </div>
-
-                    <div className={styles.utilities}>
-                        <button className={styles.utilOptions}><i className="fa-solid fa-list"></i></button>
-                        <button><i className="fa-solid fa-table-columns"></i> <span>Dashboard</span></button>
-                        <button><i className="fa-solid fa-calendar-days"></i> <span>Appointments</span></button>
-                        <button><i className="fa-solid fa-users"></i> <span>Patients</span></button>
-                        <button className={styles.activePage}><i className="fa-solid fa-user-doctor"></i> <span>Manage Doctors</span></button>
-                        <button><i className="fa-solid fa-receipt"></i>  <span>Billing</span></button>
-                    </div>
-                </div>
-
-                <div className={styles.sidebarOptions}>
-                    <button className={styles.settings}><i className="fa-solid fa-gear"></i> <span>Settings</span></button>
-                    <button className={styles.logout}><i className="fa-solid fa-arrow-right-from-bracket"></i> <span>Log Out</span></button>
-                </div>
-            </div>
+            <SideBar page="doctors" />
 
             <div className={styles.mainDiv}>
                 <div className={styles.header}>
---
+++ src/reusable_components/receptionist_sidebar/ReceptionistSideBar.jsx
@@ -0,0 +1,35 @@
+import styles from './receptionist_sidebar.module.css'
+
+export default function ReceptionistSidebar(props){
+
+    return(
+        <div className={styles.sidebar}>
+            <div className={styles.ensureTogether}>
+                <div className={styles.hospitalBranding}>
+                    <div className={styles.logo}>
+                    </div>
+
+                    <div className={styles.hospitalName}>
+                        <span><h4>Lorem ipsum dolor</h4></span>
+
+                        <span><p>Reception desk</p></span>
+                    </div>
+                </div>
+
+                <div className={styles.utilities}>
+                    <button className={styles.utilOptions}><i className="fa-solid fa-list"></i></button>
+                    <button className={props.page === "dashboard" && styles.activePage}><i className="fa-solid fa-table-columns"></i> <span>Dashboard</span></button>
+                    <button className={props.page === "appointments" && styles.activePage}><i className="fa-solid fa-calendar-days"></i> <span>Appointments</span></button>
+                    <button className={props.page === "patients" && styles.activePage}><i className="fa-solid fa-users"></i> <span>Patients</span></button>
+                    <button className={props.page === "doctors" && styles.activePage}><i className="fa-solid fa-user-doctor"></i> <span>Manage Doctors</span></button>
+                    <button className={props.page === "billing" && styles.activePage}><i className="fa-solid fa-receipt"></i>  <span>Billing</span></button>
+                </div>
+            </div>
+
+            <div className={styles.sidebarOptions}>
+                <button className={styles.settings}><i className="fa-solid fa-gear"></i> <span>Settings</span></button>
+                <button className={styles.logout}><i className="fa-solid fa-arrow-right-from-bracket"></i> <span>Log Out</span></button>
+            </div>
+        </div>
+    );
+}
