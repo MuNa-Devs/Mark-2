da06948907677767f698fd53e6f91e916c4b07b8
MuNa <devguys99@gmail.com>
2025-11-22 08:47:06 +0530

    Added root as Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
-import SignUpPage from "./pages/signup_page/SignUpPage"
-import SignInPage from "./pages/signin_page/SignInPage"
-import DashBoard from "./pages/receptionist_pages/dash_board/DashBoard"
-import DoctorManagement from "./pages/receptionist_pages/doctor_management/DoctorManagement"
+import SignUpPage from "./pages/signup_page/SignUpPage";
+import SignInPage from "./pages/signin_page/SignInPage";
+import DashBoard from "./pages/receptionist_pages/dash_board/DashBoard";
+import DoctorManagement from "./pages/receptionist_pages/doctor_management/DoctorManagement";
 
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 export default function App(){
 
     return (
         <BrowserRouter basename="/">
             <Routes>
-                <Route path="/dashboard" element={<DashBoard />}/>
+                <Route path="/" element={<SignUpPage />} />
+                
+                <Route path="/dashboard" element={<DashBoard />} />
 
                 <Route path="/doctormanagement" element={<DoctorManagement />} />
 
@@ -19,5 +21,5 @@ export default function App(){
                 <Route path="/login" element={<SignInPage />} />
             </Routes>
         </BrowserRouter>
-)
+    );
 }
